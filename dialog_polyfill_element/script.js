39db2e5f647c440770284eddf2cd148d2b946050
raielin@gmail.com <raielin@gmail.com>
2015-05-11 15:25:04 -0400

    New folder for dialog polyfill example with initial files to start with.

---
+++ dialog_polyfill_element/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// Get the dialogs.
+var dialog = document.getElementById('dialog');
+var modal = document.getElementById('modal');
+var styledModal = document.getElementById('styledModal');
+
+// Get the buttons.
+var dialogBtn = document.getElementById('showDialog');
+var modalBtn = document.getElementById('showModal');
+var styledModalBtn = document.getElementById('showStyledModal');
+var closeBtns = document.querySelectorAll('.close');
+
+// Setup event listeners for show buttons.
+dialogBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  // Show the dialog with built-in show() method
+  dialog.show();
+});
+
+modalBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  // Show modal dialog with built-in showModal() method which deactivates/locks main content behind dialog box.
+  modal.showModal();
+})
+
+styledModalBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  styledModal.showModal();
+})
+
+// Setup event listener for close buttons.
+for (var i = 0; i < closeBtns.length; i++) {
+  closeBtns[i].addEventListener('click', function(e) {
+     this.parentNode.close();
+  });
+}
