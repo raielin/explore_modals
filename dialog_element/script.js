ec1390929f87fd1b268ab069bab6ab7039ccf28a
raielin@gmail.com <raielin@gmail.com>
2015-05-11 14:20:50 -0400

    Better variable names.

--- dialog_element/script.js
+++ dialog_element/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
-// Get the dialog.
+// Get the dialogs.
 var dialog = document.getElementById('dialog');
 
 // Get the buttons.
-var showBtn = document.getElementById('show');
+var dialogBtn = document.getElementById('showDialog');
 var closeBtn = document.getElementById('close');
 
 // Setup an event listener for the show button.
-showBtn.addEventListener('click', function(e) {
+dialogBtn.addEventListener('click', function(e) {
   e.preventDefault();
 
   // Show the dialog with built-in show() method
