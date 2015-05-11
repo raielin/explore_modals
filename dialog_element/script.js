40e6ac632a130d9169d5fde79107f7a78678bf68
raielin@gmail.com <raielin@gmail.com>
2015-05-11 14:31:50 -0400

    Add script to activate modal dialog.

--- dialog_element/script.js
+++ dialog_element/script.js
@@ -2,18 +2,27 @@
 
 // Get the dialogs.
 var dialog = document.getElementById('dialog');
+var modal = document.getElementById('modal');
 
 // Get the buttons.
 var dialogBtn = document.getElementById('showDialog');
+var modalBtn = document.getElementById('showModal');
 var closeBtns = document.querySelectorAll('.close');
 
-// Setup an event listener for the show button.
+// Setup event listeners for show buttons.
 dialogBtn.addEventListener('click', function(e) {
   e.preventDefault();
 
   // Show the dialog with built-in show() method
   dialog.show();
 });
+
+modalBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  // Show modal dialog with built-in showModal() method which deactivates/locks main content behind dialog box.
+  modal.showModal();
+})
 
 // Setup event listener for close buttons.
 for (var i = 0; i < closeBtns.length; i++) {
